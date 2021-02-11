c86ae574f5296a57ab6147e3aec484ff1c1151d1
te9yie <te9yie@users.noreply.github.com>
2021-02-11 20:14:53 +0900

    Draw text

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use ggez::{
-    conf::Conf,
+    conf::{WindowMode, WindowSetup},
     event::{self, EventHandler},
+    graphics::{self, Text},
     Context, ContextBuilder, GameResult,
 };
+use glam::Vec2;
+use graphics::Font;
+use std::{env, path::PathBuf};
 
-struct State {}
+const SCREEN_SIZE: (f32, f32) = (16.0 * 40.0, 9.0 * 40.0);
+
+struct State {
+    text: Text,
+}
 
 impl State {
-    fn new() -> State {
-        State {}
+    fn new(ctx: &mut Context) -> GameResult<State> {
+        let font = Font::new(ctx, "/mplus-1p-medium.ttf")?;
+        let text = Text::new(("Hello", font, 48.0));
+        Ok(State { text })
     }
 }
 
@@ -17,17 +27,27 @@ impl EventHandler for State {
         Ok(())
     }
 
-    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
-        Ok(())
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.17, 0.17, 0.17, 1.0].into());
+        graphics::draw(ctx, &self.text, (Vec2::new(20.0, 20.0),))?;
+        graphics::present(ctx)
     }
 }
 
-fn main() {
-    let conf = Conf::new();
-    let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("hello", "te9yie")
-        .conf(conf)
-        .build()
-        .unwrap();
-    let mut state = State::new();
-    event::run(ctx, event_loop, &mut state).unwrap();
+fn main() -> GameResult {
+    let resource_dir = if let Ok(dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = PathBuf::from(dir);
+        path.push("resources");
+        path
+    } else {
+        PathBuf::from("resources")
+    };
+
+    let (mut ctx, mut event_loop) = ContextBuilder::new("hello", "te9yie")
+        .window_setup(WindowSetup::default().title("Hello"))
+        .window_mode(WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
+        .add_resource_path(resource_dir)
+        .build()?;
+    let mut state = State::new(&mut ctx)?;
+    event::run(&mut ctx, &mut event_loop, &mut state)
 }
